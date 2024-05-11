81756651a632dafafd97b6ef591fd723b940066c
Ido Cohen <94110636+MayoNChip@users.noreply.github.com>
2024-05-11 14:27:29 +0300

    added back dialog button

--- components/MyDialog.tsx
+++ components/MyDialog.tsx
@@ -17,7 +17,9 @@ type Props = {
 function MyDialog({ title, children, buttonTitle }: Props) {
   return (
     <Dialog>
-      <DialogTrigger>{/* <Button>{buttonTitle}</Button> */}</DialogTrigger>
+      <DialogTrigger>
+        <Button>{buttonTitle}</Button>
+      </DialogTrigger>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
